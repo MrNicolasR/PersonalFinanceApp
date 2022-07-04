919860fcaab3d237875accb0ed6cff5d49767421
nico <mr.n.rubert@gmail.com>
2022-07-04 14:27:44 -0400

    Initial Commit

---
+++ PersonalFinanceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonalFinanceApp
+//
+//  Created by Nicolas Rubert on 7/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonalFinanceApp/PersonalFinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceAppApp.swift
+//  PersonalFinanceApp
+//
+//  Created by Nicolas Rubert on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
